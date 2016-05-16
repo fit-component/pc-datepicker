aa089d92c36fcf9378a1a5b1a7d907e29188de65
huangziyi01 <huangziyi01@baidu.com>
2016-05-16 16:58:42 +0800

    quick push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Calendar from './calendar'
 import DateRange from './date-range'
 import DateInput from './date-input'
-import 'fit-style'
 
 export default Calendar
 export { Calendar, DateRange, DateInput }
